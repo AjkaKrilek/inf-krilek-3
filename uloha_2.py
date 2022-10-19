6a705f395811dbaf7634e17f001114bbf64cdd71
AjkaKrilek <113473323+AjkaKrilek@users.noreply.github.com>
2022-10-19 08:48:38 +0200

    Update uloha_2.py

--- uloha_2.py
+++ uloha_2.py
@@ -2,5 +2,5 @@ n = int(input('zadaj pocet:'))
 text = input('zadaj text:')
 def riadok(n:int,text):
     if text !='':
-        print('*'*n, text, '*'*n)
+        print('*'*(n/2), text, '*'*(n/2))
 print(riadok(n,text))
